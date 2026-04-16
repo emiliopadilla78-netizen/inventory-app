a97c73a22e3a95ade2e423ce7caa3d16819854e6
Emilio <emilio.padilla78@gmail.com>
2026-04-16 17:52:55 -0400

    fix login endpoint

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -5,7 +5,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-    const res = await fetch("http://localhost:3000/auth/login", {
+    const res = await fetch("/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
