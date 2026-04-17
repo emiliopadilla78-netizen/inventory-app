59af3ab5be6717c17a0a83f01fde46b4653591cb
Emilio <emilio.padilla78@gmail.com>
2026-04-16 20:38:04 -0400

    fix express 5 fallback route

--- server/static.ts
+++ server/static.ts
@@ -13,7 +13,9 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("/*", (_req, res) => {
+  app.use((req, res, next) => {
+  if (req.method !== "GET") return next();
+
   res.sendFile(path.resolve(distPath, "index.html"));
 });
 }
